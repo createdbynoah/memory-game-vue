1453ea1aa4febf616e98eb763b5b3416e7bda204
Noah Rodgers <noah@createdbynoah.com>
2023-10-06 01:30:35 -0500

    remove placeholder images, add card theme and shuffle functionality based on variables

--- src/components/cards/CardLayout.vue
+++ src/components/cards/CardLayout.vue
@@ -1,93 +1,44 @@
 <template>
   <div class="container">
     <div class="board">
-      <div v-for="image in placeholderImages">
-        <PlayingCard :images="image" :timeout="timeout" />
+      <div v-for="image in cardFrontsSet">
+        <PlayingCard
+          :frontImg="image"
+          :backImg="randomCardBack"
+          :timeout="timeout"
+          :key="image.id"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import avatars from '@/assets/data/avatarImages.json';
+import cardBackImgs from '@/assets/data/cardBackImages.json';
+import cardFrontImgs from '@/assets/data/cardFrontImages.json';
 import PlayingCard from './PlayingCard.vue';
+import calcCardSet from '@/util/cardSet.js';
 
-const placeholderImages = [
-  {
-    front: 'src/assets/images/cards/fronts/pokemon/4.png',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front: 'src/assets/images/cards/fronts/pokemon/4.png',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1545147986-a9d6f2ab03b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1545147986-a9d6f2ab03b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1520412099551-62b6bafeb5bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1520412099551-62b6bafeb5bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1584589167171-541ce45f1eea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1584589167171-541ce45f1eea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1503045666024-95470ad39b81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1503045666024-95470ad39b81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1525279844598-52fd0d3fecc2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-  {
-    front:
-      'https://images.unsplash.com/photo-1525279844598-52fd0d3fecc2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-    back: 'src/assets/images/cards/backs/pokemon.png',
-  },
-];
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]; // Swap elements at i and j
-  }
-}
-
-shuffleArray(placeholderImages);
+const randomCardBack =
+  cardBackImgs[Math.floor(Math.random() * cardBackImgs.length)];
+const theme = 'pokemon';
+const numberOfSets = 10;
+const cardsPerSet = 2;
+const cardFrontsSet = calcCardSet(
+  numberOfSets,
+  cardsPerSet,
+  cardFrontImgs[theme]
+);
 
 const timeout = 2000;
 
-const noMatch = () => {
-  setTimeout(() => {
-    showCardFront.value = false;
-  }, timeout);
-};
+// const noMatch = () => {
+//   setTimeout(() => {
+//     showCardFront.value = false;
+//   }, timeout);
+// };
 </script>
 
 <style scoped>
