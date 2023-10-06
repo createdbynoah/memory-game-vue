86e2e1f2d140e15fc549c96ba4ff1957b555ee86
Noah Rodgers <noah@createdbynoah.com>
2023-10-06 01:29:34 -0500

    update generatePaths logic

--- src/generatePaths.js
+++ src/generatePaths.js
@@ -16,50 +16,44 @@ const avatarImgDirectoryPath = path.join(__dirname, 'assets/images/avatars/');
 const basePath = path.join(__dirname, '');
 
 const getImgArray = (pathToFolder) => {
-  const imgArray = [];
+  const images = {};
 
   // Helper function to process a file:
-  // Checks if the file is an image and then adds its path to the imgArray.
   const processFile = (file, directory) => {
     if (/\.(jpg|jpeg|png|gif)$/i.test(file)) {
       let imageFilePath = path.join(directory, file);
-      // Replace absolute path with relative path starting from the project directory.
       imageFilePath = imageFilePath.replace(basePath, '/src');
-      // Extract just the parent folder name for the 'variant'.
       const variant = path.basename(directory);
-      imgArray.push({ variant: variant, image: imageFilePath });
+      if (!images[variant]) {
+        images[variant] = [];
+      }
+      images[variant].push(imageFilePath);
     }
   };
 
   // Iterate over each item (file or directory) in the given path.
   fs.readdirSync(pathToFolder).forEach((item) => {
-    // Ignore any .DS_Store files.
     if (item === '.DS_Store') return;
 
     const itemPath = path.join(pathToFolder, item);
 
     // Check if the item is a directory or a file.
     if (fs.statSync(itemPath).isDirectory()) {
-      // If it's a directory, iterate over its files and process them.
       fs.readdirSync(itemPath).forEach((file) => {
         processFile(file, itemPath);
       });
     } else {
-      // If it's a file, directly process it.
       processFile(item, pathToFolder);
     }
   });
 
-  // Return the populated imgArray.
-  return imgArray;
+  return images;
 };
 
-// Fetch image arrays for each directory.
 const cardFronts = getImgArray(cardFrontImgDirectoryPath);
 const cardBacks = getImgArray(cardBackImgDirectoryPath);
 const avatars = getImgArray(avatarImgDirectoryPath);
 
-// Write image arrays to their respective JSON files.
 fs.writeFileSync('cardFrontImages.json', JSON.stringify(cardFronts));
 fs.writeFileSync('cardBackImages.json', JSON.stringify(cardBacks));
 fs.writeFileSync('avatarImages.json', JSON.stringify(avatars));
